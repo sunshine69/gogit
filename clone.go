5d7e766f795dff447a4298f4c147dd5d431fdbed
Steve Kieu <msh.computing@gmail.com>
2022-10-27 20:44:36 +1000

    SPlit clone and implement https scheme

--- clone.go
+++ clone.go
@@ -1,33 +1,73 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net/url"
 	"os"
+	"path/filepath"
+	"strings"
 
 	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
 	u "github.com/sunshine69/golang-tools/utils"
+	gitu "github.com/whilp/git-urls"
 )
 
-func Clone(url, directory, privateKeyFile, password string) {
-	fmt.Printf("url: %s - directory: %s key: %s\n", url, directory, privateKeyFile)
+func Clone(privateKeyFile string, args []string) {
+	cloneFlag := flag.NewFlagSet("clone opt", flag.ExitOnError)
+	gitUrl := args[0]
+	var urlp *url.URL
+	var err error
+	directory := ""
 
-	_, err := os.Stat(privateKeyFile)
-	u.CheckErr(err, "privateKeyFile")
+	if len(args) > 2 && !strings.HasPrefix(args[1], "-") {
+		directory = args[1]
+	} else {
+		urlp, err = gitu.Parse(gitUrl)
+		u.CheckErr(err, "url parse")
+		// fmt.Println(urlp.Path)
+		directory = filepath.Base(urlp.Path)
+		// fmt.Println(directory)
+		directory = strings.TrimSuffix(directory, ".git")
+	}
+	// we do not have any cloneOpt yet so not in use
+	cloneFlag.Parse(args[1:])
+
+	// fmt.Printf("url: %s - directory: %s key: %s\n", gitUrl, directory, privateKeyFile)
+	username, password := "", ""
+	cloneOpt := git.CloneOptions{
+		URL:      gitUrl,
+		Progress: os.Stdout,
+	}
+	if urlp.Scheme == "https" {
+		// The token case is that username can be anything and password is the token. Fall into this code
+		username = urlp.User.Username()
+		if username != "" {
+			if _password, ok := urlp.User.Password(); ok {
+				password = _password
+			} else {
+				password = GetPassword()
+			}
+			cloneOpt.Auth = &http.BasicAuth{
+				Username: username,
+				Password: password,
+			}
+		}
+	} else {
+		_, err := os.Stat(privateKeyFile)
+		u.CheckErr(err, "privateKeyFile")
+		password = GetPassword()
+		publicKeys, err := ssh.NewPublicKeysFromFile("git", privateKeyFile, password)
+		u.CheckErr(err, "NewPublicKeysFromFile")
+		cloneOpt.Auth = publicKeys
+	}
 
 	// Clone the given repository to the given directory
-	fmt.Printf("git clone %s\n", url)
-	publicKeys, err := ssh.NewPublicKeysFromFile("git", privateKeyFile, password)
-	u.CheckErr(err, "NewPublicKeysFromFile")
+	fmt.Printf("git clone %s\n", gitUrl)
 
-	r, err := git.PlainClone(directory, false, &git.CloneOptions{
-		// The intended use of a GitHub personal access token is in replace of your password
-		// because access tokens can easily be revoked.
-		// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
-		Auth:     publicKeys,
-		URL:      url,
-		Progress: os.Stdout,
-	})
+	r, err := git.PlainClone(directory, false, &cloneOpt)
 	u.CheckErr(err, "PlainClone")
 
 	// ... retrieving the branch being pointed by HEAD
--- main.go
+++ main.go
@@ -4,13 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
 	"syscall"
 
 	git "github.com/go-git/go-git/v5"
 	u "github.com/sunshine69/golang-tools/utils"
-	gitu "github.com/whilp/git-urls"
+
 	"golang.org/x/crypto/ssh/terminal"
 	"gopkg.in/ini.v1"
 )
@@ -45,24 +43,7 @@ func main() {
 
 	switch action {
 	case "clone":
-		cloneFlag := flag.NewFlagSet("clone opt", flag.ExitOnError)
-		gitUrl := args[1]
-		password := GetPassword()
-		directory := ""
-		if len(args) > 3 && !strings.HasPrefix(args[2], "-") {
-			directory = args[2]
-		} else {
-			urlp, err := gitu.Parse(gitUrl)
-			u.CheckErr(err, "url parse")
-			fmt.Println(urlp.Path)
-			directory = filepath.Base(urlp.Path)
-			fmt.Println(directory)
-			directory = strings.TrimSuffix(directory, ".git")
-		}
-		// we do not have any cloneOpt yet so not in use
-		cloneFlag.Parse(args[1:])
-
-		Clone(gitUrl, directory, sshkeyfile, password)
+		Clone(sshkeyfile, args[1:])
 	case "commit":
 		gitUser := cfg.Section("user").Key("name").String()
 		gitUserEmail := cfg.Section("user").Key("email").String()
@@ -89,7 +70,7 @@ func GetPassword() string {
 	return string(bytePassword)
 }
 
-func GetWorkTree() (*git.Repository, *git.Worktree)  {
+func GetWorkTree() (*git.Repository, *git.Worktree) {
 	workDir, err := os.Getwd()
 	u.CheckErr(err, "workDir")
 	r, err := git.PlainOpen(workDir)
